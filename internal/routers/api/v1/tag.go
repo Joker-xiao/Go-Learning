fae1fdcf5087047fbdd6eb90ce2d4c29b2db73e7
joker_x <1605461825@qq.com>
2021-09-08 12:20:51 +0800

    code commit

---
+++ internal/routers/api/v1/tag.go
@@ -0,0 +1,27 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+type Tag struct {
+}
+
+func NewTag() Tag {
+	return Tag{}
+}
+
+func (receiver Tag) Get(c *gin.Context) {
+
+}
+
+func (receiver Tag) List(c *gin.Context) {
+
+}
+func (receiver Tag) Create(c *gin.Context) {
+
+}
+func (receiver Tag) Update(c *gin.Context) {
+
+}
+func (receiver Tag) Delete(c *gin.Context) {
+
+}
